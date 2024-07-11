5d108b4fd2394aec57458df68b950c51b71c7657
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-11 16:05:18 +0100

    file reading

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello World!");
+int main(int argc, char *argv[]) {
+    FILE *fptr = NULL;
+    char* buff = NULL;
+
+    if (argv[1] == NULL) {
+        printf("Usage: ./main.exe <path_to_your_code>");
+        return 1;
+    } 
+
+    if ((fptr = fopen(argv[1], "rb")) == NULL) {
+        perror("error opening file");
+        return 1;
+    }
+
+    fseek(fptr, 0, SEEK_END);
+    int sz = ftell(fptr);
+    fseek(fptr, 0, SEEK_SET);
+
+    buff = (char *)malloc(sz * sizeof(char));
+    fread(buff, sz, 1, fptr);
+    buff[sz] = '\0';
+
     return 0;
 }
